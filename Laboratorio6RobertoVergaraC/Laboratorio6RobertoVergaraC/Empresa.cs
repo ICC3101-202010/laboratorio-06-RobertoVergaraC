04f412052310648735e6a0e2eeb6464fbde8c142
RobertoVergaraC <ravergara@miuandes.com>
2020-05-16 19:45:53 -0400

    Parte 1

---
+++ Laboratorio6RobertoVergaraC/Laboratorio6RobertoVergaraC/Empresa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio6RobertoVergaraC
+{
+    [Serializable]
+    class Empresa
+    {
+        private string name;
+        private string rut;
+
+        public string Name { get => name; set => name = value; }
+        public string Rut { get => rut; set => rut = value; }
+
+        public Empresa() { }
+        
+        public Empresa(string name, string rut)
+        {
+            Name = name;
+            Rut = rut;
+        }
+
+
+    }
+}
--- Laboratorio6RobertoVergaraC/Laboratorio6RobertoVergaraC/Program.cs
+++ Laboratorio6RobertoVergaraC/Laboratorio6RobertoVergaraC/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Laboratorio6RobertoVergaraC
@@ -10,6 +14,70 @@ namespace Laboratorio6RobertoVergaraC
     {
         static void Main(string[] args)
         {
+            Empresa empresa = new Empresa();
+
+            while (true)
+            {
+                Console.WriteLine("Seleccione la opción que desee:");
+                Console.WriteLine("(a) Utilizar archivo existente\n(b) Crear empresa nueva\n(c) Ver información de la empresa\n(d) Salir");
+                string des = Console.ReadLine();
+                if (des == "a")
+                {
+                    IFormatter formatter = new BinaryFormatter();
+                    Stream stream = new FileStream("empresa.bin", FileMode.Open, FileAccess.Read, FileShare.Read);
+                    try
+                    {
+                        empresa = (Empresa)formatter.Deserialize(stream);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("\nLa opción seleccionada no es válida, ya que no hay ningún archivo guardado");
+                        empresa = Create();
+                    }
+                    finally
+                    {
+                        stream.Close();
+                    }
+                }
+                else if (des == "b")
+                {
+                    Create();
+                }
+                else if (des == "c")
+                {
+                    //Ver INFO
+                }
+                else if (des == "d")
+                {
+                    if (empresa.Name != null && empresa.Rut != null)
+                    {
+                        Save(empresa);
+                    }
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\nLa opción seleccionada no es válida, por favor seleccionar una que si lo sea\n");
+                }
+            }
+        }
+
+        static private Empresa Create()
+        {
+            Console.WriteLine("\nSeleccione el nombre de la empresa:");
+            string name = Console.ReadLine();
+            Console.WriteLine("\nSeleccione el rut de la empresa (Ej: 20.153.414-3)");
+            string rut = Console.ReadLine();
+            Empresa empresa = new Empresa(name, rut);
+            return empresa;
+        }
+
+        static private void Save(Empresa empresa)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream("empresa.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+            formatter.Serialize(stream, empresa);
+            stream.Close();
         }
     }
 }
